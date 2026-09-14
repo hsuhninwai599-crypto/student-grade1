4bb1a2d5b033722b2fe9f01d73ec2cd35da2c8bb
unknown <hsuhninwai599@gmail.com>
2026-09-14 14:56:14 +0630

    Add Pass/Fail result logic on GitHub

--- student-grade1/grade.py
+++ student-grade1/grade.py
@@ -5,4 +5,11 @@ mark2 = float(input("Enter second subject mark: "))
 average = (mark1 + mark2) / 2
 
 print(f"\nStudent Name: {name}")
-print(f"Average Mark: {average}")
+print(f"Average Mark: {average}"
+
+      )
+
+if average >= 50:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
